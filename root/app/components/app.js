a1e7dbb77cb7e7b3d1c8f27fdc4e088a5f951645
John S <jiwon.seoo223@gmail.com>
2019-03-13 00:15:21 -0400

    this is the first commit

---
+++ root/app/components/app.js
@@ -0,0 +1,12 @@
+//App.js
+
+import React, { Component } from 'react';
+export default class App extends Component {
+    render(){
+        return (
+            <div>
+                Hello From React v15
+            </div>
+        );
+    }
+}
